256957a710c313b14396e92d2ea30aa0e8fde9ca
mjingeaux <jingeaux.maxime@gmail.com>
2025-03-01 15:50:18 -0500

    Update grid_cell.gd

--- grid/grid_cell.gd
+++ grid/grid_cell.gd
@@ -2,7 +2,7 @@ class_name GridCell extends Resource
 
 @export var weight : int
 @export var direction : Vector2
-@export var size : int = 10
+@export var size : int = 16
 
 func _init(w:int = 0, dir:Vector2 = Vector2(0,0)) -> void:
 	weight = w
