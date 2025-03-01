290587c712ecc5eae4bfa69dea491f00b4493e96
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2025-03-01 15:29:18 -0500

    Add camera movement

--- grid_sprite.gd
+++ grid_sprite.gd
@@ -12,17 +12,21 @@ func _draw():
 	var vp_right: float = vp_size.x * camera.zoom.x
 	var vp_bottom: float = vp_size.y * camera.zoom.y
 	
-	var leftmost: float = -vp_right + cam_pos.x
-	var topmost: float = -vp_bottom + cam_pos.y
+	var leftmost: float = - (vp_size.x / camera.zoom.x + 1) * grid_size
+	var topmost: float = - (vp_size.y / camera.zoom.y + 1) * grid_size
 	
 	var left: float = ceil(leftmost / grid_size) * grid_size
-	var bottommost : float = vp_bottom + cam_pos.y
+	var bottommost : float = (vp_size.y / camera.zoom.y + 1) * grid_size
 	for x in range(0, vp_size.x / camera.zoom.x + 1):
 		draw_line(Vector2(left, topmost), Vector2(left, bottommost), color)
+		draw_line(Vector2(-left, topmost), Vector2(-left, bottommost), color)
 		left += grid_size
+	draw_line(Vector2(left, topmost), Vector2(left, bottommost), color)
 
 	var top: float = ceil(topmost / grid_size) * grid_size
-	var rightmost: = vp_right + cam_pos.x
+	var rightmost: = (vp_size.x / camera.zoom.x + 1) * grid_size
 	for y in range(0, vp_size.y / camera.zoom.y + 1):
 		draw_line(Vector2(leftmost, top), Vector2(rightmost, top), color)
+		draw_line(Vector2(leftmost, -top), Vector2(rightmost, -top), color)
 		top += grid_size
+	draw_line(Vector2(leftmost, top), Vector2(rightmost, top), color)
