4c81fe80592fc94c3e116c5cf1eee562c127f4d1
clementoune <clemgil64@gmail.com>
2025-03-02 10:54:00 -0500

    shop menu disabled buttons

--- menu/shop_menu/shop_button.gd
+++ menu/shop_menu/shop_button.gd
@@ -2,10 +2,10 @@ extends TextureButton
 
 @onready var world = get_node("/root/Main/World/")
 
-
-
+@onready var label = $Label.text
 
 var cost: int = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,9 +13,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if GlobalNode.coin_total < str_to_int(label) :
+		self.disabled = true
+	else:
+		self.disabled = false
 	pass
-
 
 func _on_pressed() -> void:
 	world.buy_object(self.name)
 	
+func str_to_int(string):
+	var regex = RegEx.new()
+	regex.compile("\\d+")
+	var all_numbers_found = regex.search_all(string)
+	for number in all_numbers_found:
+		var number_found = int(number.get_string())
+		return(number_found)
