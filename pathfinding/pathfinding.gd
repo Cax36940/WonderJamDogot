087b35a4da28a2c6d5c9b2ef7bfe586b81b11f39
mjingeaux <jingeaux.maxime@gmail.com>
2025-03-02 09:38:07 -0500

    Update weights according to objects in grid

--- pathfinding/pathfinding.gd
+++ pathfinding/pathfinding.gd
@@ -2,6 +2,7 @@ class_name PathFinding extends Node2D
 
 @onready var grid_cell_size: int = $"/root/Main/World".GRID_CELL_SIZE
 @onready var spawner = $"/root/Main/World/Spawner"
+@onready var object_container = $"/root/Main/World/GridObjectContainer"
 @onready var grid: Grid
 
 var DebugArrowScene = preload("res://pathfinding/debugarrow.tscn")
@@ -17,8 +18,6 @@ func _ready() -> void:
 			arrow.position = 16 * Vector2(x,y) + Vector2(8,8)
 			arrow.look_at(16 * Vector2(x + grid.grid[y][x].direction.x,y + grid.grid[y][x].direction.y))
 			add_child(arrow)
-	
-
 
 func _process(_delta) -> void:
 	for child in spawner.get_children():
@@ -33,3 +32,13 @@ func _process(_delta) -> void:
 		#print(child.velocity)
 		if curr_cell == grid.grid[grid.center.y][grid.center.x]:
 			child.queue_free()
+			
+func update_weights() -> void:
+	var grid_objects = object_container.get_children()
+	for object:GridObject in grid_objects:
+		var pos = object.global_position
+		var pos_grid = Vector2(int(pos.x/16),int(pos.y/16))
+		for x in range(pos_grid.x+object.width):
+			for y in range(pos_grid.y+object.height):
+				grid.update_weight(x,y,object.path_weight)
+		
