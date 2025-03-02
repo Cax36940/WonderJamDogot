57cafba3dc025879e5b15ad83d4b2b92467f58cf
Flutz17 <90649629+Flutz17@users.noreply.github.com>
2025-03-02 14:39:51 -0500

    main menu fini

--- menu/start_menu/start_menu.gd
+++ menu/start_menu/start_menu.gd
@@ -2,18 +2,23 @@ class_name StartMenu extends Control
 
 @onready var btn_start: TextureButton = $CanvasLayer/VBoxContainer/btn_start
 @onready var btn_quit: TextureButton = $CanvasLayer/VBoxContainer/btn_quit
+@onready var music_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED;
 	btn_start.pressed.connect(_on_start_pressed);
 	btn_quit.pressed.connect(_on_quit_pressed);
-	pass
+	
+	# Jouer la musique si un fichier est assigné
+	if music_player.stream:
+		music_player.play()
 
 func _on_start_pressed() -> void:
-	print("START LEVEL");
-	get_tree().paused = false;
-	queue_free();
-	
+	print("START LEVEL")
+	get_tree().paused = false
+	music_player.stop()  # Arrêter la musique quand on quitte le menu
+	queue_free()
+
 func _on_quit_pressed() -> void:
-	print("QUIT");
-	get_tree().quit();
+	print("QUIT")
+	get_tree().quit()
