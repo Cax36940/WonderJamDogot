231343417d1ad937c451b6abd814e8a245238d46
clementoune <clemgil64@gmail.com>
2025-03-02 14:18:59 -0500

    typo change + added zgloubi logo

--- menu/shop_menu/coin_count.gd
+++ menu/shop_menu/coin_count.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	$".".text = "Zgloubis : " + str(GlobalNode.coin_total)
+	$".".text = "   : " + str(GlobalNode.coin_total)
